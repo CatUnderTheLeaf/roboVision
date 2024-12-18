12369ec479e17d069bed5527c6e7ca7ea78e2bad
CatUnderTheLeaf <catundertheleaf@gmail.com>
2024-12-18 19:09:46 +0100

    load diff_drive robot model

--- bringup/launch/main.launch.py
+++ bringup/launch/main.launch.py
@@ -32,21 +32,42 @@ def generate_launch_description():
     )
     
     return LaunchDescription([
+        # launch alexa api and action servers
         IncludeLaunchDescription(
             PythonLaunchDescriptionSource([
                 PathJoinSubstitution([
-                    FindPackageShare('bringup'),
+                    FindPackageShare('voice_commands'),
                     'launch',
-                    'camera.launch.py'
+                    'alexa.launch.py'
                 ])
-            ]),
-            launch_arguments={
-                'camera_ns': camera_ns,
-                'camera_url': 'http://192.168.0.30:4747/video?320x240',
-                'camera_info_url': 'package://bringup/config/camera_info.yaml'
-            }.items()
+            ])            
         ),
 
-        yolo_detector,
-        show_detected_images
+        # launch ros2mqtt bridge
+        # IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource([
+        #         PathJoinSubstitution([
+        #             FindPackageShare('mqtt_communication'),
+        #             'launch',
+        #             'mqtt.launch.py'
+        #         ])
+        #     ])            
+        # ),
+        # IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource([
+        #         PathJoinSubstitution([
+        #             FindPackageShare('bringup'),
+        #             'launch',
+        #             'camera.launch.py'
+        #         ])
+        #     ]),
+        #     launch_arguments={
+        #         'camera_ns': camera_ns,
+        #         'camera_url': 'http://192.168.0.30:4747/video?320x240',
+        #         'camera_info_url': 'package://bringup/config/camera_info.yaml'
+        #     }.items()
+        # ),
+
+        # yolo_detector,
+        # show_detected_images
     ])
