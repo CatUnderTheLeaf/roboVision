dc61c01a849382be43e3d6b255df443363e3a978
CatUnderTheLeaf <catundertheleaf@gmail.com>
2024-12-19 21:37:14 +0100

    #22, #23

--- bringup/launch/diff_drive.launch.py
+++ bringup/launch/diff_drive.launch.py
@@ -1,50 +1,40 @@
 import launch
-from launch.substitutions import Command, LaunchConfiguration
+from launch.substitutions import Command, LaunchConfiguration, PathJoinSubstitution
 from ament_index_python.packages import get_package_share_directory
-import launch_ros
-import os
+from launch_ros.actions import Node
 
 def generate_launch_description():
+        
     pkg_project_description = get_package_share_directory('description')
-    pkg_project_bringup = get_package_share_directory('bringup')
-    default_model_path  =  os.path.join(pkg_project_description, 'models', 'diff_drive', 'model.urdf')
-    default_rviz_config_path = os.path.join(pkg_project_bringup, 'config', 'diff_drive.rviz')
-
-    robot_state_publisher_node = launch_ros.actions.Node(
+    default_model_path  =  PathJoinSubstitution([pkg_project_description, 'models', 'diff_drive', LaunchConfiguration('model')])
+  
+    robot_state_publisher_node = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}]
+        parameters=[{'robot_description': Command(['xacro ', default_model_path])}]
     )
-    joint_state_publisher_node = launch_ros.actions.Node(
+    joint_state_publisher_node = Node(
         package='joint_state_publisher',
         executable='joint_state_publisher',
         name='joint_state_publisher',
-        parameters=[{'robot_description': Command(['xacro ', default_model_path])}],
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
+        arguments=[default_model_path],
+        # parameters=[{'robot_description': Command(['xacro ', default_model_path])}],
+        # condition=launch.conditions.UnlessCondition(LaunchConfiguration('gui'))
     )
-    joint_state_publisher_gui_node = launch_ros.actions.Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
-        name='joint_state_publisher_gui',
-        condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
-    )
-    rviz_node = launch_ros.actions.Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', LaunchConfiguration('rvizconfig')],
-    )
-
+    # joint_state_publisher_gui_node = Node(
+    #     package='joint_state_publisher_gui',
+    #     executable='joint_state_publisher_gui',
+    #     name='joint_state_publisher_gui',
+    #     condition=launch.conditions.IfCondition(LaunchConfiguration('gui'))
+    # )
+    
     return launch.LaunchDescription([
-        launch.actions.DeclareLaunchArgument(name='gui', default_value='True',
-                                            description='Flag to enable joint_state_publisher_gui'),
-        launch.actions.DeclareLaunchArgument(name='model', default_value=default_model_path,
+        # launch.actions.DeclareLaunchArgument(name='gui', default_value='False',
+        #                                     description='Flag to enable joint_state_publisher_gui'),
+        launch.actions.DeclareLaunchArgument(name='model', default_value='model.xacro',
                                             description='Absolute path to robot urdf file'),
-        launch.actions.DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
-                                            description='Absolute path to rviz config file'),
+
         joint_state_publisher_node,
-        joint_state_publisher_gui_node,
+        # joint_state_publisher_gui_node,
         robot_state_publisher_node,
-        rviz_node
     ])
