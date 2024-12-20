cbdf68859e34ce8e287fcbf4d069e22f377356d5
CatUnderTheLeaf <catundertheleaf@gmail.com>
2024-12-20 19:34:09 +0100

    update from gazebo classic to gazebo fortress

--- bringup/launch/simulation.launch.py
+++ bringup/launch/simulation.launch.py
@@ -1,7 +1,7 @@
 from launch_ros.substitutions import FindPackageShare
 
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess, DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription, ExecuteProcess, DeclareLaunchArgument, AppendEnvironmentVariable
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch_ros.actions import Node
@@ -12,9 +12,47 @@ def generate_launch_description():
     
     arg_model_name = 'gazebo.xacro'
     pkg_project_bringup = get_package_share_directory('bringup')
+    ros_gz_sim = get_package_share_directory('ros_gz_sim')
+
+    world = PathJoinSubstitution([
+                FindPackageShare('description'),
+                'worlds',
+                'small_house.world'
+            ])
+
+
+    set_env_vars_resources = AppendEnvironmentVariable(
+            'GZ_SIM_RESOURCE_PATH',
+            PathJoinSubstitution([
+                get_package_share_directory('description'),
+                'models'
+            ])
+    )
+
+    gzserver_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                ros_gz_sim,
+                'launch',
+                'gz_sim.launch.py'
+            ])
+        ),
+        launch_arguments={'gz_args': ['-r -s -v4 ', world], 'on_exit_shutdown': 'true'}.items()
+    )
+    gzclient_cmd = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            PathJoinSubstitution([
+                ros_gz_sim,
+                'launch',
+                'gz_sim.launch.py'
+            ])
+        ),
+        launch_arguments={'gz_args': '-g -v4 '}.items()
+    )
+
   
     spawn_entity = Node(
-        package='gazebo_ros',
+        package='ros_gz_sim',
         executable='spawn_entity.py',
         arguments=['-entity', 'lego_bot', '-topic', 'robot_description'],
         output='screen'
@@ -29,35 +67,45 @@ def generate_launch_description():
            PathJoinSubstitution([pkg_project_bringup, 'config/ekf.yaml']), {'use_sim_time': LaunchConfiguration('use_sim_time')}]
     )
 
+
+
     return LaunchDescription([
-        DeclareLaunchArgument(name='use_sim_time', default_value='True',
-                            description='Flag to enable use_sim_time'),
-        ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so'], output='screen'),
-        spawn_entity,
 
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                PathJoinSubstitution([
-                    FindPackageShare('bringup'),
-                    'launch',
-                    'diff_drive.launch.py'
-                ])
-            ]),
-            launch_arguments={
-                'model': arg_model_name,
-                'rviz': 'False'
-            }.items()
-        ),
+        set_env_vars_resources,
+        gzserver_cmd,
+        gzclient_cmd
 
-        robot_localization_node,
 
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([
-                PathJoinSubstitution([
-                    FindPackageShare('bringup'),
-                    'launch',
-                    'rviz.launch.py'
-                ])
-            ]),            
-        ),
+
+        # DeclareLaunchArgument(name='use_sim_time', default_value='True',
+        #                     description='Flag to enable use_sim_time'),
+        # ExecuteProcess(cmd=['gazebo', '--verbose', '-s', 'libgazebo_ros_init.so', '-s', 'libgazebo_ros_factory.so', world], output='screen'),
+        
+        # spawn_entity,
+
+        # IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource([
+        #         PathJoinSubstitution([
+        #             FindPackageShare('bringup'),
+        #             'launch',
+        #             'diff_drive.launch.py'
+        #         ])
+        #     ]),
+        #     launch_arguments={
+        #         'model': arg_model_name,
+        #         'rviz': 'False'
+        #     }.items()
+        # ),
+
+        # robot_localization_node,
+
+        # IncludeLaunchDescription(
+        #     PythonLaunchDescriptionSource([
+        #         PathJoinSubstitution([
+        #             FindPackageShare('bringup'),
+        #             'launch',
+        #             'rviz.launch.py'
+        #         ])
+        #     ]),            
+        # ),
     ])
